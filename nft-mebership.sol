ccc89149cdb2c387eb6ce8b6429f1e643e986322
Sungyu Kang <gron1gh1@gmail.com>
2022-01-26 19:23:37 +0900

    Div: NFT Contract, Membership Contract & Add: Burn

--- nft-mebership.sol
+++ nft-mebership.sol
@@ -1,68 +1,43 @@
-pragma solidity >=0.4.24 <=0.5.6;
+pragma solidity 0.8.0;
 
+contract IKIP17 {
+    mapping(uint256 => address) private tokenOwner;
+    mapping(address => uint256) public ownerById;
 
-contract NFT_Membership {
-    struct Membership {
-        string value; 
-        uint256 expirationDate;
-    }
-
-    address private contractOwner;
-    // 소유한 토큰 리스트
-    mapping(uint256 => address) public tokenOwner;
-    mapping(uint256 => Membership) public tokenMembership;
     bytes4 private constant _KIP17_RECEIVED = 0x6745782b;
 
-    // mint(tokenId, uri, owner)
-    // transferFrom(from, to, tokenId) -> owner가 바뀌는 것(from -> to)
-
-    constructor() public {
-        contractOwner = msg.sender;
-    }
-
-    function mint(
+    function _mint(
         address to,
         uint256 tokenId
-    ) private returns (bool) {
-        require(contractOwner == msg.sender, "컨트랙트를 배포한 사람만 민팅이 가능합니다.");
-        // to에게 tokenId(일련번호)를 발행하겠다.
-        // 적힐 글자는 tokenURI
+    ) internal returns (bool) {
         tokenOwner[tokenId] = to;
+        ownerById[to] = tokenId;
         return true;
     }
     
-    
-    function mintWithMembership(
-        address to,
-        uint256 tokenId,
-        string memory value,
-        uint256 expirationDate
-    ) public {
-        require(contractOwner == msg.sender, "컨트랙트를 배포한 사람만 민팅이 가능합니다.");
-        tokenMembership[tokenId] = Membership(value,expirationDate);
-        mint(to,tokenId);
-    }
-
-    function burn(uint256 tokenId) public {
-        require(contractOwner == msg.sender, "컨트랙트를 배포한 사람만 소각이 가능합니다.");
+    function _burn(uint256 tokenId) internal {
+        require(
+            msg.sender == tokenOwner[tokenId],
+            "You not token Owner"
+        );
+        delete ownerById[msg.sender];
         delete tokenOwner[tokenId];
-        delete tokenMembership[tokenId];
     }
 
 
-    function safeTransferFrom(
-        address from,
+
+    function _safeTransferFrom(
         address to,
         uint256 tokenId,
         bytes memory _data
-    ) public {
+    ) internal {
         require(
-            from == tokenOwner[tokenId],
-            "해당 토큰을 소지한 사람만 전송이 가능합니다."
+            msg.sender == tokenOwner[tokenId],
+            "You not token Owner"
         );
         tokenOwner[tokenId] = to;
         require(
-            _checkOnKIP17Received(from, to, tokenId, _data),
+            _checkOnKIP17Received(msg.sender, to, tokenId, _data),
             "KIP17: transfer to non KIP17Receiver implementer"
         );
     }
@@ -110,5 +85,43 @@ contract NFT_Membership {
             size := extcodesize(account)
         }
         return size > 0;
+    }
+}
+
+contract Membership is IKIP17 {
+    struct Membership {
+        string value; 
+        uint256 expirationDate;
+    }
+    address private contractOwner;
+    
+    mapping(uint256 => Membership) public tokenMembership;
+
+    constructor() public {
+        contractOwner = msg.sender;
+    }
+    
+    // mintWithMembership(발급할 주소, 토큰 아이디, 토큰 값, 현재시간 기준 달 설정)
+    function mintWithMembership(
+        address to,
+        uint256 tokenId,
+        string memory value,
+        uint256 expirationMonth
+    ) public {
+        require(contractOwner == msg.sender, "You not admin"); //, "컨트랙트를 배포한 사람만 민팅이 가능합니다."
+        tokenMembership[tokenId] = Membership(value, block.timestamp + expirationMonth * 30 days);
+        _mint(to,tokenId);
+    }
+
+    function burn(uint256 tokenId) private {
+        delete tokenMembership[tokenId];
+        _burn(ownerById[msg.sender]);
+    }
+
+
+    function showMembership() public view returns (Membership memory)  {
+        require(ownerById[msg.sender] != 0, "Not find Membership"); //, "소유한 회원권이 없습니다."
+         require(block.timestamp <= tokenMembership[ownerById[msg.sender]].expirationDate, "This Membership End"); // , "회원권의 기간이 지났습니다."
+        return tokenMembership[ownerById[msg.sender]];
     }
 }
